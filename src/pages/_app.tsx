42b153e071709615327d9b53ce70bec50bbadf19
shahbaz-f22labs <shahbaz@f22labs.com>
2023-05-08 16:37:16 +0530

    removed unwanted files nd code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import type { AppProps } from "next/app";
-import Layout from "@/components/organisms/layout";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
