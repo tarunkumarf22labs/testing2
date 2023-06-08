40455e3b30e309446807af6bace700aa35ae4cb1
shahbaz-f22labs <shahbaz@f22labs.com>
2023-06-08 14:46:06 +0530

    Fix: fixed the position of the banner text

--- src/stories/Carousal.tsx
+++ src/stories/Carousal.tsx
@@ -10,6 +10,7 @@ interface CarousalProps {
   images: string[];
 }
 import { Autoplay } from "swiper";
+import Search from "./Search";
 
 export default function Carousal(props: CarousalProps) {
   const { images } = props;
@@ -34,16 +35,17 @@ export default function Carousal(props: CarousalProps) {
                   width={1920}
                   height={1080}
                   alt="Poster Image"
-                  className=" h-[410px] sm:h-[500px] md:h-[810px]"
+                  className=" h-[410px] sm:h-[500px] md:h-[650px] lg:h-[810px]"
                 />
               </div>
             </SwiperSlide>
           );
         })}
       </Swiper>
-      <div className="Home-page-text-div absolute top-[50%] left-[50%] z-[48] w-1/2 md:w-[50%] lg:1/3 xl:w-[30%] text-4xl font-[Brandon Grotesque] tracking-wide text-white">
+      <div className="Home-page-text-div absolute top-[35%] sm:top-[30%] left-[50%] z-[48] w-1/2 md:w-[50%] lg:1/3 xl:w-[30%] text-4xl font-[Brandon Grotesque] tracking-wide text-white">
         <p>UNLOCK THE LUXURY WITH LUXUNLOCK</p>
       </div>
+
     </div>
   );
 }
