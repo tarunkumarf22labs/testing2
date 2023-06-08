66ca85d2220ec8684b3b0249e17fd7556d44958c
shahbaz-f22labs <shahbaz@f22labs.com>
2023-06-08 14:13:00 +0530

    Fix: banner image display bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,16 @@ import Layout from "@/components/Layout";
 import type { NextPage } from "next";
 import Carousal from "src/stories/Carousal";
 import { HomeBannerimages } from "src/data/constants";
+import Search from "src/stories/Search";
 
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout title="LuxUnlock">
+           <>
            <Carousal images={HomeBannerimages} />
+           </>
       </Layout>
     </>
   );
