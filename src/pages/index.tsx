80171c9d58fc78d36b887f9570de77298047605f
shahbaz-f22labs <shahbaz@f22labs.com>
2023-06-12 13:08:44 +0530

    FEAT: CU-85zt3nf16 bulit the carousal component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,22 @@ import Carousal from "src/stories/Carousal";
 import { HomeBannerimages } from "src/data/constants";
 import Search from "src/stories/Search";
 
-
 const Home: NextPage = () => {
+  const bannerImageStyle = "h-[410px] sm:h-[500px] md:h-[650px] lg:h-[810px]";
+  const bannerTextStyle =
+    "text-[#F8F8F9] absolute top-[35%] sm:top-[30%] left-[50%] z-[48] w-1/2 md:w-[50%] xl:w-[45%]";
+  const bannerText = "UNLOCK THE LUXURY WITH LUXUNLOCK";
   return (
     <>
       <Layout title="LuxUnlock">
-           <>
-           <Carousal images={HomeBannerimages} />
-           </>
+        <>
+          <Carousal
+            images={HomeBannerimages}
+            bannerImageStyle={bannerImageStyle}
+            bannerTextStyle={bannerTextStyle}
+            bannerText={bannerText}
+          />
+        </>
       </Layout>
     </>
   );
--- src/stories/Carousal.tsx
+++ src/stories/Carousal.tsx
@@ -8,15 +8,14 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 interface CarousalProps {
   images: string[];
+  bannerImageStyle: string;
+  bannerTextStyle: string;
+  bannerText: string;
 }
 import { Autoplay } from "swiper";
-import Search from "./Search";
 
-export default function Carousal(props: CarousalProps) {
-  const text = 'UNLOCK THE LUXURY WITH LUXUNLOCK'
-  const { images } = props;
-
-  
+export default function Carousal({ images,bannerImageStyle,bannerTextStyle,bannerText }: CarousalProps) {
+    
   return (
     <div className="relative text-center md:-mt-16">
       <Swiper
@@ -38,38 +37,15 @@ export default function Carousal(props: CarousalProps) {
                   width={1920}
                   height={1080}
                   alt="Poster Image"
-                  className=" h-[410px] sm:h-[500px] md:h-[650px] lg:h-[810px]"
+                  className={bannerImageStyle}
                 />
               </div>
             </SwiperSlide>
           );
         })}
       </Swiper>
-      <div className="overflow-hidden Home-page-text-div absolute top-[35%] sm:top-[30%] left-[50%] z-[48] w-1/2 md:w-[50%] lg:1/3 xl:w-[30%] text-4xl font-[Brandon Grotesque] tracking-wide text-white">
-        <motion.p className="animate-slide-down">UNLOCK THE LUXURY WITH LUXUNLOCK</motion.p>
-        {/* {text.split('').map((letter, index) => (
-        <motion.span
-          key={index}
-          // initial={{ opacity: 0, y: -50 }}
-          // animate={{ opacity: 1, y: 0 }}
-          // transition={{ delay: index * 0.1, duration: 0.5 }}
-
-          
-          // style={{ display: 'inline-block', marginRight: '4px' }}
-          // initial={{ y: -50, opacity: 0 }}
-          // animate={{ y: 0, opacity: 1 }}
-          // transition={{ delay: index * 0.1, duration: 0.5 }}
-
-          style={{ display: 'inline-block', marginRight: '4px' }}
-            initial={{ y: -50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: index * 0.1, duration: 0.1 }}
-            exit={{ y: 50, opacity: 0 }}
-        >
-          {letter}
-        </motion.span>
-      ))} */}
-
+      <div className={`overflow-hidden ${bannerTextStyle} Home-page-text-div font-[Brandon Grotesque] tracking-wide text-4xl md:text-5xl`}>
+        <p className="capitalize animate-slide-down">{bannerText}</p>
       </div>
 
     </div>
