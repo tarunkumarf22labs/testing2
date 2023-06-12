6ac1d6c7a3e9c5792bd6e9b6a8a851730ce081a7
shahbaz-f22labs <shahbaz@f22labs.com>
2023-06-12 19:14:37 +0530

    FEAT:CU-85zt3h2y4 built the search component

--- src/stories/Search.tsx
+++ src/stories/Search.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Datepicker from "./DatePicker";
-// import DatePicker from "./DatePicker";
-import {
-  DatePickerStateProvider,
-  useContextCalendars,
-  useContextDays,
-  useContextMonthsPropGetters,
-} from "@rehookify/datepicker";
+import Link from "next/link";
 import { motion } from "framer-motion";
+import { AppContext } from "src/Context";
 const visible = { opacity: 1, y: 0, transition: { duration: 0.5 } };
 const variants = {
   hidden: { opacity: 0 },
@@ -21,7 +16,7 @@ const variants = {
 
 const testVariants = {
   animate: {
-    y:'10vh',
+    y: "10vh",
     opacity: 2,
   },
   initial: {
@@ -31,55 +26,55 @@ const testVariants = {
     type: "spring",
     // duration: 2
     stiffness: 60,
-    damping: 100
-  }
-}
+    damping: 100,
+  },
+};
 
 const Search = () => {
   const [showDate, setShowDate] = useState(false);
-  const [startDate, setStartDate] = useState<string | undefined>("");
-  const [endDate, setEndDate] = useState<string | undefined>("");
+  const { startDate, endDate } = useContext(AppContext);
 
   useEffect(() => {
     let datesFlag = (!startDate && !endDate) || (startDate && endDate);
-   
+
     datesFlag ? setShowDate(false) : setShowDate(true);
-    
   }, [endDate, startDate]);
 
   return (
     <>
       <motion.div
-        style={{opacity: 0.2}}
+        style={{ opacity: 0.2 }}
         // variants={testVariants}
         animate={{
-            // y: "10vh",
-            opacity: 1,
-            // backgroundColor:"blue",
-            // scale: 2,
-            // rotate: isAnimating ? 360 : 0
+          // y: "10vh",
+          opacity: 1,
+          // backgroundColor:"blue",
+          // scale: 2,
+          // rotate: isAnimating ? 360 : 0
         }}
         initial={{
-            opacity: 0
+          opacity: 0,
         }}
         transition={{
-            // type: "spring",
-            duration: 3
-            // stiffness: 60,
-            // damping: 100
+          // type: "spring",
+          duration: 3,
+          // stiffness: 60,
+          // damping: 100
         }}
-      className="bg-[#FFFFFF] -mt-[50px] relative m-auto z-[60]  border md:w-11/12 lg:flex md:justify-between md:items-center  lg:w-full xl:w-9/12">
+        className="bg-[#FFFFFF] -mt-[50px] relative m-auto z-[60]  border md:w-11/12 lg:flex md:justify-between md:items-center  lg:w-full xl:w-9/12 font-[Brandon Grotesque] text-[#7B8084]"
+      >
         <div className="sm:flex justify-evenly items-center lg:w-[52%]">
           <div className="mb-8 ml-3 mr-3 border-b-2 sm:w-5/12 md:mb-11">
             <select
               name=""
               id=""
               placeholder="Select Destination"
-              className="w-full border border-b-2 border-none outline-none mt-7 "
+              className="w-full border border-b-2 border-none outline-none mt-7 focus:ring-0 custom-select"
             >
-              <option value="Chennai">Chennai</option>
-              <option value="Delhi">Delhi</option>
-              <option value="Mumbai">Mumbai</option>
+              
+              <option value="Chennai" >Chennai</option>
+              <option value="Delhi" >Delhi</option>
+              <option value="Mumbai" >Mumbai</option>
             </select>
           </div>
           <div
@@ -103,11 +98,10 @@ const Search = () => {
             </svg>
           </div>
         </div>
-        <div
-          className="sm:flex justify-evenly items-center lg:w-[52%]"
-        >
-          <div className="flex items-center justify-between mt-8 ml-3 mr-3 border-b-2 sm:w-5/12 md:m-0" 
-          onClick={() => setShowDate(!showDate)}
+        <div className="sm:flex justify-evenly items-center lg:w-[52%]">
+          <div
+            className="flex items-center justify-between mt-8 ml-3 mr-3 border-b-2 sm:w-5/12 md:m-0"
+            onClick={() => setShowDate(!showDate)}
           >
             {endDate ? <p>{endDate}</p> : <p>CHECK OUT</p>}
             <svg
@@ -126,32 +120,18 @@ const Search = () => {
             </svg>
           </div>
           <div className="flex items-center justify-between mb-4 ml-3 mr-3 border-b-2 sm:w-5/12 md:mb-11">
-            <select
-              name=""
-              id=""
-              placeholder="Select Destination"
-              className="w-full border border-b-2 border-none outline-none mt-7 "
-            >
-                {Array.from({ length: 20 }, (_, index) => (
-                  <option key={index + 1} value={index + 1} className="-mb-2">{index + 1}</option>
-                ))}
-            </select>
+            <input type="number" className="w-full border-none mt-7 focus:ring-0" placeholder="Guest" min={1}/>
           </div>
         </div>
 
         <div className="bg-[#8A1E61] flex justify-between items-center ml-3 mr-3 mb-4 h-12 sm:mt-5 md:w-11/12 md:-mt-4 md:m-auto md:mb-2 lg:w-[10%] lg:mr-[3%]">
           <button className="bg-[#8A1E61] text-white text-center m-auto h-full font-[Brandon Grotesque]">
-            {" "}
-            SEARCH{" "}
+            <Link href={"/chloe"}>SEARCH</Link>
           </button>
         </div>
       </motion.div>
       <div className={`${showDate === false ? "hidden" : "block"}`}>
         <Datepicker
-          startDate={startDate}
-          setStartDate={setStartDate}
-          endDate={endDate}
-          setEndDate={setEndDate}
           inVillaDetails={false}
         />
       </div>
@@ -160,4 +140,3 @@ const Search = () => {
 };
 
 export default Search;
-
