cc682d5179d74a0b5511965b7f202dd8d966cdaa
shahbaz-f22labs <shahbaz@f22labs.com>
2023-06-12 22:27:41 +0530

    worked on Villa Banner

--- src/stories/VillaBanner.tsx
+++ src/stories/VillaBanner.tsx
@@ -2,31 +2,26 @@ import React from "react";
 import Image from "next/image";
 
 interface VillaBannerInterface {
-  title: string;
+  bannerText: string;
+  bannerTextStyle: string;
+  bannerImageStyle: string;
 }
 
 export const VillaBanner = (props: VillaBannerInterface) => {
-    const { title } = props
-    console.log(title)
+    const { bannerText,bannerTextStyle,bannerImageStyle } = props
+    console.log(bannerText)
   return (
-    <div className="w-full h-[40vh]  border sm:h-[80vh]">
-      {/* <Image
+    <div className="relative text-center md:-mt-16">
+      <Image
         src={"/images/a01.png"}
         alt="Villa Poster"
-        width={100}
-        height={100}
-      /> */}
-         {/* <Image
-      src="/images/Logo.png"
-      width={138}
-      height={26}
-      alt="Lux Unlock Logo"
-      className="md:w-40 md:h-8"
-    /> */}
-            <div className="font-light hidden w-64 md:w-96 absolute  top-2/4 left-1/4 sm:block md:left-[40%] text-center text-4xl font-[Brandon Grotesque] tracking-wide z-50">
-         <p> {title}</p>
+        width={1920}
+        height={1080}
+        className={bannerImageStyle}
+      />
+     <div className={`overflow-hidden ${bannerTextStyle} Home-page-text-div font-[Brandon Grotesque] tracking-wide text-4xl md:text-5xl`}>
+        <p className="capitalize animate-slide-down">{bannerText}</p>
       </div>
-     
     </div>
   );
 };
