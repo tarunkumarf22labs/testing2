a2423ce28dc314a8df367852d9012a2f568ab85f
shahbaz-f22labs <shahbaz@f22labs.com>
2023-06-14 13:17:59 +0530

    FEAT: CU-85zt9pbvu-villa-card created searched Villa card

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -178,3 +178,22 @@ export const HomeBannerimages = [
   "/images/a07.png",
   "/images/a08.png",
 ];
+
+// shahbaz
+
+export const SearchedresultVillaData = {
+  amenities : [
+    "6 Guest",
+    "3 Bedrooms",
+    "2 Bathrooms",
+    "English-Style Cottage",
+  ],
+  city: 'Coonoor',
+  state: 'Tamil Nadu',
+  title:'Chole Cottage',
+  description: " In a village overlooking the backwaters of the Vembanad Lake near Cochin, experience an unimaginably perfect abode under the stars and reflected in water. Here is a beautiful and soulful luxury home in Kerala that invites you to live in the moment. Built by one of India's best-known ad-film makers as a passion project, with incredible attention to detail.",
+  available: false,
+  availableDates: '28 Jun - 31 MAY',
+  villaPrice: '₹12,589',
+  roomPrice: "₹5,589"
+}
---
+++ src/pages/result.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SearchedVillaCard from "src/stories/SearchedVillaCard";
+import { SearchedresultVillaData } from "src/data/constants";
+
+const result = () => {
+  return (
+    <div>
+      <SearchedVillaCard
+        amenities={SearchedresultVillaData.amenities}
+        city={SearchedresultVillaData.city}
+        state={SearchedresultVillaData.state}
+        title={SearchedresultVillaData.title}
+        description={SearchedresultVillaData.description}
+        available={SearchedresultVillaData.available}
+        availableDates={SearchedresultVillaData.availableDates}
+        villaPrice={SearchedresultVillaData.villaPrice}
+        roomPrice={SearchedresultVillaData.roomPrice}
+      />
+    </div>
+  );
+};
+
+export default result;
