b24d14356187f853094c52124ea0726a25af4323
shahbaz-f22labs <shahbaz@f22labs.com>
2023-06-14 15:19:04 +0530

    FIX: CU-85zt9pbvu added box shadow and small image

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -6,6 +6,8 @@ const result = () => {
   return (
     <div>
       <SearchedVillaCard
+      bannerImage={SearchedresultVillaData.bannerImage}
+      image={SearchedresultVillaData.image}
         amenities={SearchedresultVillaData.amenities}
         city={SearchedresultVillaData.city}
         state={SearchedresultVillaData.state}
