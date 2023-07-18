06a314e0e2ba2629bb1391ea68cb4cbf6ffab132
shahbaz-f22labs <shahbaz@f22labs.com>
2023-07-18 19:46:24 +0530

    FEAT: cu-85ztjbfu2-view-all-amenities built only 20 lists will be visible initially

--- src/stories/AmenitiesSection.tsx
+++ src/stories/AmenitiesSection.tsx
@@ -24,7 +24,7 @@ export const AmenitiesSection = ({
   const maxEightIcons =
     iconsArray.length > 9 ? iconsArray.slice(0, 9) : iconsArray;
   const [showAmenities, setShowAmenites] = useState(false);
- 
+
   return (
     <div className="flex justify-between h-auto py-10 mx-5 xl:py-20 xl:mx-auto xl:max-w-7xl">
       <div>
@@ -35,34 +35,44 @@ export const AmenitiesSection = ({
           <p className="text-3xl font-light">Amenities</p>
         </div>
         <div className="space-y-7 xl:hidden text-[#545456]">
-          {maxEightIcons?.map((icon, index) => (
-            <p key={index} className="flex">
-              {/* {icons[icon.icon]}{" "} */}
-              <Image
-                src={icon.image.image}
-                width={20}
-                height={20}
-                alt={icon.image.alt}
-              />
-              <span className="pl-3 text-base font-centaur">{icon.text}</span>
-            </p>
-          ))}
+          {maxEightIcons?.map((icon, index) => {
+            if (index < 20) {
+              return (
+                <p key={index} className="flex">
+                  {/* {icons[icon.icon]}{" "} */}
+                  <Image
+                    src={icon.image.image}
+                    width={20}
+                    height={20}
+                    alt={icon.image.alt}
+                  />
+                  <span className="pl-3 text-base font-centaur">
+                    {icon.text}
+                  </span>
+                </p>
+              );
+            }
+          })}
         </div>
         <div className="hidden xl:grid md:grid-cols-2 gap-y-6 md:max-w-3xl md:min-w-[700px] text-[#545456]">
-          {iconsArray?.map((icon, index) => (
-            <p key={index} className="flex items-center ">
-              {/* {icons[icon.icon]}{" "} */}
-              <Image
-                src={icon.image.image}
-                width={20}
-                height={20}
-                alt={icon.image.alt}
-              />
-              <span className="pl-3 text-base font-centaur max-w-[288px]">
-                {icon.text}
-              </span>
-            </p>
-          ))}
+          {iconsArray?.map((icon, index) => {
+            if (index < 20) {
+              return (
+                <p key={index} className="flex items-center ">
+                  {/* {icons[icon.icon]}{" "} */}
+                  <Image
+                    src={icon.image.image}
+                    width={20}
+                    height={20}
+                    alt={icon.image.alt}
+                  />
+                  <span className="pl-3 text-base font-centaur max-w-[288px]">
+                    {icon.text}
+                  </span>
+                </p>
+              );
+            }
+          })}
         </div>
         {isMobile || iconsArray.length > 20 ? (
           <div
@@ -74,12 +84,7 @@ export const AmenitiesSection = ({
         ) : null}
       </div>
       <div className="hidden md:block relative h-[673px]">
-        <Image
-          src={portraitImage}
-          alt="amenities1"
-          width={462}
-          height={673}
-        />
+        <Image src={portraitImage} alt="amenities1" width={462} height={673} />
         <Image
           // src="/images/Amenities2.png"
           src={thumbnailImage}
